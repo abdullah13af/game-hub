c160ac57aacb641629f7b871171fc85734af77f2
Abdullah Anwar <abdullah.13af@gmail.com>
2023-03-29 21:31:14 +0500

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import {Card, CardBody, CardHeader, Heading, Image, Text} from "@chakra-ui/react";
+import {Card, CardBody, CardHeader, Heading, HStack, Image, Text} from "@chakra-ui/react";
 import React from "react";
 import {Game} from "../hooks/useGame";
+import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
@@ -13,7 +14,10 @@ function GameCard({game}: Props) {
 			<Image src={game.background_image} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
-				<PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+				<HStack justifyContent="space-between">
+					<PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+					<CriticScore score={game.metacritic} />
+				</HStack>
 			</CardBody>
 		</Card>
 	);
